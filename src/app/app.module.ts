5dc37d4f63fe42dd0a6dcaa12de775ca1ef740e7
Arantxa <arantxa.martinezpiquer@gmail.com>
2018-06-06 19:57:55 +0100

    Fix libraries' versions and typo in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
       FormsModule,
       HttpModule,
-      MAterialModule,
+      MaterialModule,
       FlexLayoutModule
   ],
   providers: [],
